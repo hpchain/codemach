ee9edd84325612b9f6b7c977b79cbbca26ebd7fb
黄文海 <huangwenhai@bubi.cn>
2018-11-15 18:47:28 +0800

    fxi Edge userAgent

--- src/components/webkitWarn.vue
+++ src/components/webkitWarn.vue
@@ -32,9 +32,9 @@ export default {
       var isOpera = userAgent.indexOf("Opera") > -1; // 判断是否Opera浏览器  
       var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera; // 判断是否IE浏览器  
       var isEdge = userAgent.indexOf("Windows NT 6.1; Trident/7.0;") > -1 && !isIE; // 判断是否IE的Edge浏览器  
-      var isFF = userAgent.indexOf("Firefox") > -1; // 判断是否Firefox浏览器  
+      var isFF = userAgent.indexOf("Firefox") > -1; // 判断是否Firefox浏览器
       var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1; // 判断是否Safari浏览器  
-      var isChrome = userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1; // 判断Chrome浏览器  
+      var isChrome = userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Edge") == -1; // 判断Chrome浏览器  
       if(isIE) {
         var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
         reIE.test(userAgent);
